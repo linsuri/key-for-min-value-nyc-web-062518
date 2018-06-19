1aa7ea1021e245120bbf28fa4f441d44a013a789
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-19 19:22:59 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   current_value = 0
   name_hash.each do |name, hash|
     current_value = hash
-    if hash <= current_value
+    if hash < current_value
       output = name
     end  
   end  
